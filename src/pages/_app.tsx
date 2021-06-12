8bcd17c4162a46dd4b619d2a66ff1e51a3a83765
TakayukiTooyama <t.tooyama107@gmail.com>
2021-06-12 13:39:37 +0900

    chakra ui 導入

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
 import { NextPage } from 'next'
 import { AppProps } from 'next/dist/next-server/lib/router/router'
+import { ChakraProvider } from '@chakra-ui/react'
 import Head from 'next/head'
+import { theme } from 'styles/theme'
 
 const MyApp: NextPage<AppProps> = ({ Component, pageProps }: AppProps) => {
   return (
@@ -8,7 +10,9 @@ const MyApp: NextPage<AppProps> = ({ Component, pageProps }: AppProps) => {
       <Head>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      <Component {...pageProps} />
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
     </>
   )
 }
